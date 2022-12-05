c4377fa47d82b237c5303443913d7eac6aaa0906
Jordan Cieslinski <jordan@cieslinskis.com>
2022-12-04 22:35:42 -0500

    day3 finished

--- Day4/part2.py
+++ Day4/part2.py
@@ -1,14 +1,18 @@
 file = open('input.txt', 'r')
 list = file.readlines()
 
-def contain(str):
+
+
+# 2-8, 3-7
+# 3-7, 2-8
+def findRename(str):
     a = int(str.split(',')[0].split('-')[0])
     b = int(str.split(',')[0].split('-')[1])
     c = int(str.split(',')[1].split('-')[0])
     d = int(str.split(',')[1].split('-')[1])
-    return a <= c and b >= d or a >= c and b <= d
+    return c >= a and c <= b or d >= a and d <= b or a >= c and a <=d or b >= c and b <=d
 sum = 0
 for line in list:
-    if contain(line):
+    if findRename(line):
         sum = sum + 1
 print(sum)

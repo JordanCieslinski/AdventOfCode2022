b11e7e5aaf2119810ae4f63f1a531d49a280e0fa
Jordan Cieslinski <jordan@cieslinskis.com>
2022-12-08 15:52:05 -0500

    finished day6

--- Day6/part2.py
+++ Day6/part2.py
@@ -3,8 +3,8 @@ list = file.readline()
 
 def findnorepeats():
     bool = False
-    for i in range(4, len(list)):
-        str = list[i-4:i]
+    for i in range(14, len(list)):
+        str = list[i-14:i]
         for j in str:
             if str.count(j) > 1:
                 bool = True

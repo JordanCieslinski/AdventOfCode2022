7d20e5203d559ddde76108093ccf56da81f9e74f
Jordan Cieslinski <jordan@cieslinskis.com>
2022-12-09 00:09:54 -0500

    working on 7-1

--- Day7/part1.py
+++ Day7/part1.py
@@ -12,9 +12,19 @@ class node:
             st += child.__str__(level+1)
         return st
 
+
+# Need to first split up each chunk of ls btw cds, then get to the directory and use recursion to load in each node
+# Eventually will need to computer total storage
+
 root = node('/', 0)
 root.children = []
 curDir = None
-for line in list:
-    if line[0:4] == '$ cd':
-        print()
+for i in range(2, len(list)):
+    if list[i][0:4] == '$ cd':
+        break
+    if list[i][0:3] == 'dir':
+        root.children.append(node(list[i][4:len(list[i])-1], 0))
+    else:
+        st = list[i].split()
+        root.children.append(node(st[1], st[0]))
+print(root)

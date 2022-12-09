0736dc82507dd1cd08be6ea9c549c3ff8c5a28d6
Jordan Cieslinski <jordan@cieslinskis.com>
2022-12-08 23:25:12 -0500

    working on day 7

--- Day7/part1.py
+++ Day7/part1.py
@@ -1,2 +1,20 @@
 file = open('./test.txt', 'r')
-list
+list = file.readlines()
+
+class node:
+    def __init__(self, data, size, children = []):
+        self.data = data
+        self.size = size
+        self.children = children
+    def __str__(self, level=0):
+        st = '\t'*level+repr(self.data)+' '+str(self.size)+'\n'
+        for child in self.children:
+            st += child.__str__(level+1)
+        return st
+
+root = node('/', 0)
+root.children = []
+curDir = None
+for line in list:
+    if line[0:4] == '$ cd':
+        print()
